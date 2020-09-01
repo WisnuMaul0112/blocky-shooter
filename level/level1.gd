70261437ec3c379d9674208c5ff2c0764833fdba
WisnuMaul0112 <mwisnu543@gmail.com>
2020-09-01 13:18:06 +0700

    animasi fade

--- level/level1.gd
+++ level/level1.gd
@@ -3,6 +3,10 @@ extends Node2D
 var b = Global.bullet
 var next_stage = "res://level/level2.tscn"
 var main_menu = "res://menu/HomeScreen.tscn"
+var menu = false
+var stage = false
+onready var colorreactblack = $CanvasLayer/ColorRect
+onready var animasi_fade =$AnimationPlayer 
 onready var popup = $CanvasLayer/popup
 onready var Bullet = $CanvasLayer/Label
 
@@ -10,6 +14,7 @@ func _ready():
 	b = 3
 	Bullet.text = "Bullet: " + str(b)
 	popup.hide()
+	colorreactblack.hide()
 
 func _on_player_nembak():
 	Bullet.text = "Bullet: " + str(Global.bullet)
@@ -24,4 +29,13 @@ func _on_popup_next_stage():
 	get_tree().change_scene(next_stage)
 
 func _on_popup_main_menu():
-	get_tree().change_scene(main_menu)
+	menu = true
+	animasi_fade.play("fade")
+	colorreactblack.show()
+	#get_tree().change_scene(main_menu)
+
+
+func animasi_fade(anim_name):
+	if menu == true:
+		get_tree().change_scene(main_menu)
+		
