46223239ee7cb723bb81bf49262647becbc8e620
WisnuMaul0112 <mwisnu543@gmail.com>
2020-09-02 22:32:02 +0700

    popup level 5

--- level/level4.gd
+++ level/level4.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
 export var jumlah_spawn = 1
+var exit = false
+var next_level = false
 var musuh = preload("res://enemy/enemy.tscn")
 var b = Global.bullet
+var next_stage = "res://level/level5.tscn"
+var home_screen = "res://menu/HomeScreen.tscn"
+onready var animasi = $CanvasLayer/AnimationPlayer
+onready var black = $CanvasLayer/ColorRect
+onready var popup = $CanvasLayer/popup_win
 
 func _ready():
 	b = 3
+	black.hide()
+	popup.hide()
 	$CanvasLayer/bullet.text = "Bullet: " + str(b)
 	musuh
 
@@ -30,3 +39,31 @@ func _on_TambahPeluru_isi_peluru():
 func _on_portal_world():
 	pass 
 
+func menang():
+	var animasi_popup = $CanvasLayer/popup_win/AnimationPlayer
+	popup.show()
+	animasi_popup.play("slidepopup")
+	get_tree().paused = true
+	
+	
+
+
+func main_menu():
+	exit = true
+	animasi.play("fade")
+	black.show()
+
+
+func next_stage():
+	next_level = true
+	animasi.play("fade")
+	black.show()
+
+
+func animation_finished(anim_name):
+	if exit == true :
+		get_tree().change_scene(home_screen)
+		get_tree().paused = false
+	elif next_level == true:
+		get_tree().change_scene(next_stage)
+		get_tree().paused = false
